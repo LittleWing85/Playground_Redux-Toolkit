b0b2a20c43a859043554a9f5680ecdbd168ab49b
littlewing <mail@alexandra-jaeger.net>
2022-12-12 23:21:06 +0100

    Backup current code; question open

--- src/features/currentPokemon/CurrentPokemon.js
+++ src/features/currentPokemon/CurrentPokemon.js
@@ -1,10 +1,10 @@
 import { useSelector } from "react-redux";
 
 export function CurrentPokemon() {
-    /* const currentPokemon = useSelector((state) => state.currentPokemon); */
+    const currentPokemon = useSelector((state) => state.currentPokemon);
     return (
         <div>
-            {/* <p>{currentPokemon.name}</p> */}
+            <p>{currentPokemon.name}</p>
             <p>Here you will see the name of the current Pokemon soon</p>
         </div>
     );
